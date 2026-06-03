047095a3ec20454299e1b742431f9ae91e66c9fc
aggingg <gwenbonequinhalindinha@gmail.com>
2026-06-03 08:39:46 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import s from './App.module.css'
 import { useEffect, useState } from 'react'
 import { api } from './constants/api'
+import {Card} from './components/card'
 
-import logo from '/logo.webp'
+import logo from '/logo.png'
 
 
 function App() {
   const [data, setData] = useState([])
-  const [page, setPage] = useState()
+  const [page, setPage] = useState(1)
 
 
   useEffect(() => {
 
-    api.get(`/character/?page=${page}`).then((response) => {
-      setData(response.data.results)
+    api.get(`/characters`).then((response) => {
+      console.log(response.data.items)
+      setData(response.data.items)
     }).catch((error) => {
       console.error("Caiu foi tudo meu fi")
     })
@@ -24,13 +26,13 @@ function App() {
     <>
     <div>
       <label>Digite uma página</label>
-      <input min={1} max={42} type="number" placeholder='1/42' value={page} onChange={(e) => setPage(Number(e.target.value))}/>
+      <input min={1} max={58} type="number" placeholder='1/58' value={page} onChange={(e) => setPage(Number(e.target.value))}/>
     </div>
       <img className={s.logo} src={logo} alt="Logo" />
       <main>
-        {data.map((item, index) => {
+        {data.map((item) => {
         return(
-          <Card key={item.id} unico={item.id} imagem={item.image} nome={item.name} espécie={item.species} origem={item.origin.name}/>
+          <Card unico={item.id} imagem={item.image} nome={item.name} ki={item.ki} maxKi={item.maxKi} raça={item.race} genero={item.gender} />
         )
         })}
       </main>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,9 +5,10 @@ export const Card = (props) => {
         <div key={props.id}>
           <img src={props.imagem} alt={props.nome} />
           <h4>Name: {props.nome}</h4>
-          <p>Species: {props.especies}</p>
-          {item.status === "Dead" ? "Status: 🧟" : item.status === "Alive" ? "Status: ❤️" : <p>Status: {props.status}  </p>} 
-          <p>Origin: {props.origin.name}</p>
+          <p>Ki: {props.ki}</p>
+          <p>MaxKi: {props.maxKi}  </p>
+          <p>Race: {props.raça}</p>
+          <p>Gender: {props.genero}</p>
         </div>
       )
 
